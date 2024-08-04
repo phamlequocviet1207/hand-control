1e0f7cb98e493bd38f3d29429c2665ca44b8da64
Viete <phamlequocviet1207@gmail.com>
2024-08-04 09:12:07 -0400

    visualize the volumn bar

--- HandControl.py
+++ HandControl.py
@@ -2,6 +2,8 @@ import cv2
 import mediapipe as mp
 import math
 
+import numpy as np
+
 drawingModule = mp.solutions.drawing_utils
 handsModule = mp.solutions.hands
 
@@ -14,9 +16,13 @@ volume = interface.QueryInterface(IAudioEndpointVolume)
 # volume.GetMute()
 # volume.GetMasterVolumeLevel()
 volRange = volume.GetVolumeRange()
-volume.SetMasterVolumeLevel(0, None)
+# volume.SetMasterVolumeLevel(0, None)
 minVol = volRange[0]
 maxVol = volRange[1]
+
+vol = 0
+volBar = 400
+volPer = 0
 
 cap = cv2.VideoCapture(0)
 
@@ -72,13 +78,24 @@ with handsModule.Hands(static_image_mode=False, min_detection_confidence=0.7, mi
 
                     # finding the length between 2 points
                     length = math.sqrt((x4-x8)**2 + (y4-y8)**2)
-                    print(length)
+                    # print(length)
+
+                    # Hand Range: 50-300
+                    # Volumn Range: -65, 0
+                    vol = np.interp(length, [50,300], [minVol,maxVol])
+                    volBar = np.interp(length, [50,300], [400,150])
+                    volPer = np.interp(length, [50, 300], [0, 100])
+                    print(int(length), vol)
+                    volume.SetMasterVolumeLevel(vol, None)
 
                     if length > 50:
                         cv2.line(frame, (x4, y4), (x8, y8), (0, 0, 255), 5)
                     else:
                         cv2.line(frame, (x4, y4), (x8, y8), (0, 255, 0), 5)
 
+            cv2.rectangle(frame, (50,150), (85,400), (0,255,0), 3)
+            cv2.rectangle(frame, (50, int(volBar)), (85,400), (0,255,0), cv2.FILLED)
+            cv2.putText(frame, f'{int(volPer)}%', (40,450), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 3)
 
         cv2.imshow('frame', frame)
 
